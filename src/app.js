83bfd5dc5cfeb61d914a42af57c2ce3957071637
Paulo Sergio <pauloserg861@gmail.com>
2022-09-07 12:01:29 -0300

    instalando e habilitando cors

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ const express = require('express')
 const db = require('./db')
 const booksRouter = require('./routes/books.router')
 const authorsRouter = require('./routes/authors.router')
+const cors = require('cors')
 const app = express()
 
 db.dbConnect()
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(cors())
 
 app.use('/api', booksRouter)
 app.use('/api', authorsRouter)
