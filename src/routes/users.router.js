b4454639c6385660d12122beffade36909cdf95a
Paulo Sergio <pauloserg861@gmail.com>
2022-09-12 21:09:21 -0300

    ajustando rotas

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -4,12 +4,12 @@ const jwt = require('../middlewares/auth-middleware')
 
 router.get('/', (req, res) => {res.send({ message: 'rendizando diretor do arquivo routes' })})
 
-router.get('/usuarios/GET/:id?', UsersController.getUsers)
-//caminho de busca por query: http://localhost:3000/api/livros/busca/GET?editora=Buzz
-router.get('/usuarios/busca/GET', UsersController.getUserByName)
-router.post('/usuarios/POST', UsersController.createUser)
-router.post('/usuarios/auth', UsersController.login)
-router.delete('/usuarios/DELETE/:id', UsersController.removeUser)
+router.get('/usuarios:id?', UsersController.getUsers)
+//caminho de busca por query: http://localhost:3000/api/livros/busca?editora=Buzz
+router.get('/usuarios/busca', UsersController.getUserByName)
+router.post('/usuarios', UsersController.createUser)
+router.post('/usuariosauth', UsersController.login)
+router.delete('/usuarios/:id', UsersController.removeUser)
 
 //rota provada para teste
 router.put('/usuarios/PUT/:id', jwt.authMiddleware, UsersController.updateUser)
