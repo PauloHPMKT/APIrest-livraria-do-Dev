4520f979c1cc85501edb7bf6a87c15e8308be13a
Paulo Sergio <pauloserg861@gmail.com>
2022-09-24 13:22:26 -0300

    implementando metodos de crud e rotas

--- src/controllers/sessions.controller.js
+++ src/controllers/sessions.controller.js
@@ -23,7 +23,27 @@ async function getSession(req, res) {
     res.status(200).json(session)
 }
 
+async function updateSession(req, res) {
+    const { id } = req.params
+
+    const session = await SessionsModel.findByIdAndUpdate(
+        { _id: id }, req.body, { new: true }
+    )
+
+    res.send({ message: 'sessao atualizada', session })
+}
+
+async function removeSession(req, res) {
+    const { id } = req.params
+
+    const removeSession = await SessionsModel.deleteOne({ _id: id })
+
+    res.send({ message: 'sessao excluida', removeSession })
+}
+
 module.exports = {
     createSession,
     getSession,
+    updateSession,
+    removeSession,
 }
--- src/routes/sessions.router.js
+++ src/routes/sessions.router.js
@@ -4,21 +4,14 @@ const jwt = require('../middlewares/auth-middleware')
 
 router.get('/session', SessionsController.getSession)
 router.post('/session', SessionsController.createSession)
-router.put('/session')
-router.delete('/session')
+router.put('/session', SessionsController.updateSession)
+router.delete('/session', SessionsController.removeSession)
 
 module.exports = router
 
-/*router.get('/', (req, res) => {res.send({ message: 'rendizando diretor do arquivo routes' })})
+/*
+lembrar de bloquear rotas após os testes com a API
 
-router.get('/usuarios:id?', UsersController.getUsers)
-//caminho de busca por query: http://localhost:3000/api/livros/busca?editora=Buzz
-router.get('/usuarios/busca', UsersController.getUserByName)
-router.post('/usuarios', UsersController.createUser)
-router.post('/usuariosauth', UsersController.login)
-router.delete('/usuarios/:id', UsersController.removeUser)
-
-//rota provada para teste
 router.put('/usuarios/PUT/:id', jwt.authMiddleware, UsersController.updateUser)
 
 module.exports = router*/
