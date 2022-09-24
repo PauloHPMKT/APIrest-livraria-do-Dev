bcb91d3f47bf8af0e8169c1b9034d1691b96337d
Paulo Sergio <pauloserg861@gmail.com>
2022-09-24 12:08:47 -0300

    instanciando rota principal de sessoes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 require('dotenv').config()
 const express = require('express')
 const db = require('./db')
+//routers
 const booksRouter = require('./routes/books.router')
 const authorsRouter = require('./routes/authors.router')
 const usersRouter = require('./routes/users.router')
+const sessionsRouter = require('./routes/sessions.router')
+//api instances
 const cors = require('cors')
 const app = express()
 
@@ -16,6 +19,7 @@ app.use(cors())
 app.use('/api', booksRouter)
 app.use('/api', authorsRouter)
 app.use('/api', usersRouter)
+app.use('/api', sessionsRouter)
 
 
 module.exports = app
