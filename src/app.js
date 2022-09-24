7ac4cc8e80d45c8939ef477dede9b0339e777e54
Paulo Sergio <pauloserg861@gmail.com>
2022-09-24 14:18:10 -0300

    organizando estrutura da api

--- src/app.js
+++ src/app.js
@@ -1,21 +1,25 @@
 require('dotenv').config()
 const express = require('express')
 const db = require('./db')
-//routers
+const cors = require('cors')
+const app = express()
+
+//routers imports
 const booksRouter = require('./routes/books.router')
 const authorsRouter = require('./routes/authors.router')
 const usersRouter = require('./routes/users.router')
 const sessionsRouter = require('./routes/sessions.router')
 //api instances
-const cors = require('cors')
-const app = express()
 
+//db connection
 db.dbConnect()
 
+//set api
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors())
 
+//routes
 app.use('/api', booksRouter)
 app.use('/api', authorsRouter)
 app.use('/api', usersRouter)
