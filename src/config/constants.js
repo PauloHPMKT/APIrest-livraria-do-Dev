8e104f9f4f25683019e7a951268d5e312fd961de
Paulo Sergio <pauloserg861@gmail.com>
2022-10-12 20:06:30 -0300

    criando arquivo de constants

---
+++ src/config/constants.js
@@ -0,0 +1,12 @@
+module.exports = {
+    httpStatusCode: {
+        OK: 200,
+        CREATED: 201,
+        BAD_REQUEST: 400,
+        UNAUTHORIZED: 401,
+        FORBIDEN: 403,
+        NOT_FOUND: 404,
+        INTERNAL_SERVER_ERROR: 500,
+        BAD_GATEWAY: 502,
+    }
+}
