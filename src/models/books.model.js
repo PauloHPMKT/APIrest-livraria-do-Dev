c1a83b1c81167e05bb093bc5b59acc82ff71f62a
Paulo Sergio <pauloserg861@gmail.com>
2022-10-15 14:38:54 -0300

    adicionando poster ao schema de livros

--- src/models/books.model.js
+++ src/models/books.model.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const schema = new mongoose.Schema({
     cod: Number,
     title: String,
+    poster: String,
     author: { type: mongoose.Schema.Types.ObjectId, ref: 'authors' }, //fazendo referencia de uma outra collection
     publishing: { type: mongoose.Schema.Types.ObjectId, ref: 'publishing' },
     plot: String,
