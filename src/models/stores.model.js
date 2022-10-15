589f8fa10c7d54efffbc44e18e4f75f37c345175
Paulo Sergio <pauloserg861@gmail.com>
2022-10-15 15:33:40 -0300

    criando schema de lojas

---
+++ src/models/stores.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+    store_id: Number,
+    name: String,
+    location: {
+        address: {
+            street: String,
+            city: String,
+            state: String,
+            zipcode: String,
+        },
+        geo: {
+            type: String,
+            coordinates: [],
+        },
+    },
+    createdAt: { type: Date, default: Date.now() }
+})
+
+const StoresModel = mongoose.model('store', schema)
+
+module.exports = StoresModel
