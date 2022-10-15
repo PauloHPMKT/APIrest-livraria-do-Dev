e5d2a36e81fc497946a3b94768df0eb42d8f6453
Paulo Sergio <pauloserg861@gmail.com>
2022-10-14 22:12:58 -0300

    implementando campos para o schema de livros

--- src/models/books.model.js
+++ src/models/books.model.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema({
-    titulo: String,
-    autor: { type: mongoose.Schema.Types.ObjectId, ref: 'authors' }, //fazendo referencia de uma outra collection
-    editora: String,
-    genero: String,
-    createAt: { type: Date, default: Date.now() }
+    cod: Number,
+    title: String,
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'authors' }, //fazendo referencia de uma outra collection
+    publishing: { type: mongoose.Schema.Types.ObjectId, ref: 'publishing' },
+    plot: String,
+    full_plot: String, 
+    genres: String,
+    language: String,
+    year: Number,
+    pages_number: Number,
+    createdAt: { type: Date, default: Date.now() }
 })
 
 const BooksModel = mongoose.model('books', schema)
