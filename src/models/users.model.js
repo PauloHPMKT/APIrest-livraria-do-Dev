5e5ee9e82396b86d09574a4b79536b5ecf1fdec4
Paulo Sergio <pauloserg861@gmail.com>
2022-10-22 21:56:17 -0300

    acrescentando mais fields ao schema de usuarios

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -2,8 +2,17 @@ const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema({
     name: String,
+    nick_name: 'String',
+    sex: String,
     email: String,
     password: String,
+    location: {
+        address: {
+            street: String,
+            city: String,
+            state: String,
+        },
+    },
     createAt: { type: Date, default: Date.now() }
 })
 
