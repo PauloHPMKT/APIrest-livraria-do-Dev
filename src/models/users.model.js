0774b27a163e9e86885aa3c75117e4f4e42eaf70
Paulo Sergio <pauloserg861@gmail.com>
2022-12-10 21:07:09 -0300

    corrigindo  retornos da requisicao

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -1,21 +1,23 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema({
-    name: String,
-    nick_name: 'String',
-    sex: String,
-    email: String,
-    password: String,
-    location: {
-        address: {
-            street: String,
-            city: String,
-            state: String,
-        },
-    },
-    createAt: { type: Date, default: Date.now() }
-})
+	name: String,
+	nick_name: "String",
+	sex: String,
+	email: String,
+	password: String,
+	location: {
+		address: {
+			street: String,
+			city: String,
+			state: String,
+		},
+	},
+	createAt: { type: Date, default: Date.now },
+});
 
-const UsersModel = mongoose.model('users', schema)
+const UsersModel = mongoose.model("users", schema);
 
-module.exports = UsersModel
+module.exports = UsersModel;
+
+//https://demos.creative-tim.com/soft-ui-dashboard/pages/dashboard.html - frontend
