101d0e843164a0cbfe6dc1c01a4432bc0c821fca
Paulo Sergio <pauloserg861@gmail.com>
2022-12-16 22:25:06 -0300

    instalando e configurando multer

--- src/app.js
+++ src/app.js
@@ -1,34 +1,49 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const morgan = require('morgan')
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const path = require("path");
 
-const db = require('./db')
+//node instance
+const app = express();
+
+const db = require("./db");
 //routers imports
-const booksRouter = require('./routes/books.router')
-const authorsRouter = require('./routes/authors.router')
-const usersRouter = require('./routes/users.router')
-const sessionsRouter = require('./routes/sessions.router')
-const storesRouter = require('./routes/store.router')
+const booksRouter = require("./routes/books.router");
+const authorsRouter = require("./routes/authors.router");
+const usersRouter = require("./routes/users.router");
+const sessionsRouter = require("./routes/sessions.router");
+const storesRouter = require("./routes/store.router");
 
 //db connection
-db.dbConnect()
+db.dbConnect();
 
 //set morgan
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 
 //set api
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(cors())
+app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+/*
+setting cors manually
+
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*')
+	res.setHeader('Access-Control-Allow-Methods', '*')
+	res.setHeader('Access-Control-Allow-Headers', '*')
+
+	next()
+})
+*/
 
 //routes
-app.use('/api', booksRouter)
-app.use('/api', authorsRouter)
-app.use('/api', usersRouter)
-app.use('/api', sessionsRouter)
-app.use('/api', storesRouter)
+app.use("/api", booksRouter);
+app.use("/api", authorsRouter);
+app.use("/api", usersRouter);
+app.use("/api", sessionsRouter);
+app.use("/api", storesRouter);
 
-
-module.exports = app
+module.exports = app;
--- src/middlewares/auth-middleware.js
+++ src/middlewares/auth-middleware.js
@@ -1,27 +1,36 @@
-const jwt = require('jsonwebtoken')
-const { httpStatusCode, throwNewError, successStatus } = require('../config/constants')
+const jwt = require("jsonwebtoken");
+const {
+	httpStatusCode,
+	throwNewError,
+	successStatus,
+} = require("../config/constants");
 
 function authMiddleware(req, res, next) {
-    const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(" ")[1]
+	const authHeader = req.headers["authorization"];
+	const token = authHeader && authHeader.split(" ")[1];
 
-    if(!token) {
-        return res.status(httpStatusCode.UNAUTHORIZED).json({ message: throwNewError.ACCESS_DENIED.message })
-    }
+	if (!token) {
+		return res
+			.status(httpStatusCode.UNAUTHORIZED)
+			.json({ message: throwNewError.ACCESS_DENIED.message });
+	}
 
-    try {
-        const secret = process.env.SECRET_KEY
-        jwt.verify(token, secret)
+	try {
+		const secret = process.env.SECRET_KEY;
+		jwt.verify(token, secret);
 
-        next()
+		next();
+	} catch (error) {
+		res
+			.status(httpStatusCode.UNAUTHORIZED)
+			.json({ message: throwNewError.INVALID_TOKEN.message });
+	}
 
-    } catch (error) {
-        res.status(httpStatusCode.UNAUTHORIZED).json({ message: throwNewError.INVALID_TOKEN.message })
-    }
-    
-    res.status(httpStatusCode.OK).json({ message: successStatus.FREE_ACCESS.message })
+	res
+		.status(httpStatusCode.OK)
+		.json({ message: successStatus.FREE_ACCESS.message });
 }
 
 module.exports = {
-    authMiddleware,
-}
+	authMiddleware,
+};
