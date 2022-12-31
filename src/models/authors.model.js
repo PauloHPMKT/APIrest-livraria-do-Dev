81e8195e29e4713a26bf0d6a8a9088b846a7a19f
Paulo Sergio <pauloserg861@gmail.com>
2022-12-31 20:22:56 -0300

    reestruturando collection de authores

--- src/models/authors.model.js
+++ src/models/authors.model.js
@@ -1,10 +1,11 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema({
-    name: String,
-    nacionalidade: String,
-})
+	name: String,
+	nacionality: String,
+	biography: String,
+});
 
-const AuthorsModel = mongoose.model('authors', schema)
+const AuthorsModel = mongoose.model("authors", schema);
 
-module.exports = AuthorsModel
+module.exports = AuthorsModel;
