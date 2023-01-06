7237eafd119e6f9d5168110c0b0e348106e483da
Paulo Sergio <pauloserg861@gmail.com>
2023-01-06 16:34:33 -0300

    criando rota de remove upload

--- src/routes/upload.router.js
+++ src/routes/upload.router.js
@@ -2,11 +2,13 @@ const router = require("./index");
 const {
 	getUploadCover,
 	uploadCover,
+	removeUpload,
 } = require("../controllers/uploads.controller");
 const upload = require("../middlewares/upload-middleware");
 
 //rotas de upload de imagem
 router.get("/books/upload/:id", getUploadCover);
 router.post("/books/upload", upload.single("image"), uploadCover);
+router.delete("/books/upload/:id", removeUpload);
 
 module.exports = router;
