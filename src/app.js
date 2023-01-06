1da4a068301aba7e3f98c6c3337ca32b35ae3b70
Paulo Sergio <pauloserg861@gmail.com>
2023-01-05 21:25:06 -0300

    habilitando api para form-data

--- src/app.js
+++ src/app.js
@@ -35,6 +35,7 @@ app.use((req, res, next) => {
 	res.setHeader("Access-Control-Allow-Origin", "*");
 	res.setHeader("Access-Control-Allow-Methods", "*");
 	res.setHeader("Access-Control-Allow-Headers", "*");
+	res.setHeader("Content-Type", "multipart/form-data");
 
 	next();
 });
