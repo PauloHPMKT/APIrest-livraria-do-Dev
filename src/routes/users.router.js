82c299dd3e9d62e999bb339e155ba122dd37c26a
Paulo Sergio <pauloserg861@gmail.com>
2023-01-09 22:39:37 -0300

    criando rota paginada

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -7,11 +7,13 @@ router.get("/", (req, res) => {
 });
 
 router.get("/users:id?", UsersController.getUsers);
+// url de paginacao: http://localhost:3005/api/users/paginated?page=#&limit=#
+router.get("/users/paginated?", UsersController.getPaginatedUsers);
 //caminho de busca por query: http://localhost:3000/api/livros/busca?editora=Buzz
 router.get("/user/busca", UsersController.getUserByName);
 router.post("/user/register", UsersController.createUser);
 router.post("/usuariosauth", UsersController.login);
-router.delete("/usuarios/:id", UsersController.removeUser);
+router.delete("/user/:id", UsersController.removeUser);
 
 //rota provada para teste lembrar de bloquear rota com middleware
 router.patch("/usuarios/:id", UsersController.updateUser);
